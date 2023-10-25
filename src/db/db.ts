8b22781e2bf3c8d6601cae72b6569ca7a2a3b396
Tanvir Hossain Sadi <htanvir.sadi@gmail.com>
2023-10-25 13:56:59 +0600

    fix(database): updated mongodb url

--- src/db/db.ts
+++ src/db/db.ts
@@ -2,9 +2,9 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config()
 
-// const MONGO_URI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jnizw.mongodb.net/Folder-Structure?retryWrites=true&w=majority`
+const MONGO_URI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jnizw.mongodb.net/Folder-Structure?retryWrites=true&w=majority`
 
-const MONGO_URI = `mongodb://localhost:27017/folder-structure`;
+// const MONGO_URI = `mongodb://localhost:27017/folder-structure`;
 
 // mongodb connection
 mongoose.connect(MONGO_URI)
